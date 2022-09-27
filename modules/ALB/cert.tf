8e63b0e3012a02e008a914fd8ec134886d31a885
Nick <godwinjulin@gmail.com>
2022-09-27 16:13:00 +0100

    update

--- modules/ALB/cert.tf
+++ modules/ALB/cert.tf
@@ -30,10 +30,11 @@ resource "aws_route53_record" "project_19_record" {
   zone_id         = data.aws_route53_zone.project_19_zone.zone_id
 }
 
+
 # validate the certificate through DNS method
-resource "aws_acm_certificate_validation" "project_19_validation" {
-  certificate_arn         = aws_acm_certificate.project_19_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.project_19_record : record.fqdn]
+resource "aws_acm_certificate_validation" "practicelab" {
+  certificate_arn         = aws_acm_certificate.practicelab.arn
+  validation_record_fqdns = [for record in aws_route53_record.practicelab : record.fqdn]
 }
 
 # create records for tooling
